477aada22e120ec497dd721ff198d9113be8bdb3
matmenzl <info@mathiasmenzl.ch>
2016-10-23 10:54:03 +0200

    optimized styling of detail-pagefooter and filter-output

--- footer.php
+++ footer.php
@@ -1,22 +1,24 @@
     <footer>
+    <div class="container">
       <div class="row">
       <a class="scrollup" href="#" style="display:inline;">Scroll</a>
 
-      <div class="col-md-12 col-lg-12">
-        <div class="col-md-6 col-lg-6">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+        <div class="col-sm-6 col-md-6 col-lg-6">
           <div class="footerleft">
             <div class="copyright">
               &copy; <?php echo date("Y"); echo " "; echo bloginfo('name'); ?> 
             </div>      
           </div> 
         </div>
-        <div class="col-md-6 col-lg-6">
+        <div class="col-sm-6 col-md-6 col-lg-6">
           <div class="footerright">
             Website by <a href="http://www.mathiasmenzl.ch"><i class="fa fa-heart" aria-hidden="true"></i>|<i class="fa fa-code" aria-hidden="true"></i>|<i class="fa fa-headphones" aria-hidden="true"></i></a>
           </div>
         </div>
         </div>
       </div><!-- .row -->
+      </div><!-- container -->
     </footer>
   </body>
 </html>
