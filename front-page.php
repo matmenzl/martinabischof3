f5f1a7f2ce04c2c6cd69aa8c824aaa23be5b7601
matmenzl <info@mathiasmenzl.ch>
2017-06-26 20:27:44 +0200

    finised front-page.php

--- front-page.php
+++ front-page.php
@@ -2,9 +2,12 @@
 <?php get_header(); ?>
 
 
-<div class="frontimage">
+<div class="frontpage">
+
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+
 
               <?php the_title(); ?>
               <?php the_content(); ?>
@@ -12,7 +15,7 @@
         <?php endwhile; ?>
 
       <?php endif; ?>
-
+      <div class="frontbrand">Martina Bischof</div>
       <button class="refresh" style="background-color:transparent;border:none;"><a href="" onclick="dummy(0);return false;" ><span class="glyphicon glyphicon-chevron-right chevronicon" aria-hidden="true"></span></a></button>
 
 
